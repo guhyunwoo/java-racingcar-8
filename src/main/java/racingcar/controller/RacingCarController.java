f61838ac2e9a70ee4378ed84ea9c8608ae6bd195
guhyunwoo <realization0327@gmail.com>
2025-10-24 08:43:43 +0900

    feat: 자동차 이름 입력 기능 구현
    - 입력 메시지를 출력한 뒤 콘솔에서 문자열 입력을 받는 readInput() 메서드 구현

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,11 +1,15 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class RacingCarController {
     public void playRacing() {
-        readInput(InputMessage.CAR_NAME_INPUT_MESSAGE);
+        String carNameCsv = readInput(InputMessage.CAR_NAME_INPUT_MESSAGE);
     }
 
     private String readInput(InputMessage inputMessage) {
         System.out.println(inputMessage.content);
+        String inputString =  Console.readLine();
+        return inputString;
     }
 }
