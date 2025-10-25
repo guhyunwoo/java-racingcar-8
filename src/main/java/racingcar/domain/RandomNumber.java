f1adf684908f7af138fcffdeedefae8c9361ab17
guhyunwoo <realization0327@gmail.com>
2025-10-25 21:31:12 +0900

    refactor: 기존 난수 생성 클래스 Random을 Randoms로 수정
    코스 요구사항에 맞게 난수 생성 클래스를 Random에서 코스에서 제공하는 라이브러리인 Randoms로 수정
    
    - RANDOM_NUMBER_BOUND를 START_INCLUSIVE, END_INCLUSIVE로 분리
    - 0~9 범위를 명시적으로 표현

--- src/main/java/racingcar/domain/RandomNumber.java
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -1,15 +1,15 @@
 package racingcar.domain;
 
-import java.util.Random;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumber {
     private final Integer value;
-    private static final Random random = new Random();
-    private static final Integer RANDOM_NUMBER_BOUND = 10;
+    private static final Integer START_INCLUSIVE = 0;
+    private static final Integer END_INCLUSIVE = 9;
     private static final Integer CRITERION = 4;
 
     public RandomNumber() {
-        this.value = random.nextInt(RANDOM_NUMBER_BOUND);
+        this.value = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
 
     public Boolean isMoreThanCriterion() {
