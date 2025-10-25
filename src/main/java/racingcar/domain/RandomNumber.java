085213c375b5a0b9a58b803158a37b439353bb98
guhyunwoo <realization0327@gmail.com>
2025-10-25 18:25:27 +0900

    refactor: 기존 랜덤 숫자 생성 책임을 RandomNumber로 위임
    - RandomNumberGenerator 클래스 제거
    - RandomNumber 클래스 개선
      * Random 인스턴스를 내부에서 직접 관리 (static final)
      * RANDOM_NUMBER_BOUND 상수 추가 (10)
      * CRITERION 상수를 static으로 관리
      * 불필요한 의존성 제거로 구조 단순화

--- src/main/java/racingcar/domain/RandomNumber.java
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -1,12 +1,15 @@
 package racingcar.domain;
 
+import java.util.Random;
+
 public class RandomNumber {
     private final Integer value;
-    private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-    private final Integer CRITERION = 4;
+    private static final Random random = new Random();
+    private static final Integer RANDOM_NUMBER_BOUND = 10;
+    private static final Integer CRITERION = 4;
 
     public RandomNumber() {
-        this.value = randomNumberGenerator.generateRandomNumber();
+        this.value = random.nextInt(RANDOM_NUMBER_BOUND);
     }
 
     public Boolean isMoreThanCriterion() {
